a43aebf49f1d19e2d7bb135787edc332e325a682
SebastianH-ctrl <61822376+SebastianH-ctrl@users.noreply.github.com>
2023-05-19 11:23:31 +0200

    Added more tests

--- tests/browser.test.js
+++ tests/browser.test.js
@@ -26,6 +26,14 @@ test('The stack should be empty in the beginning', async () => {
 	expect(stack).toEqual("n/a");
 });
 
+/* NYTT TEST*/
+test('Clicking "Poppa från stacken" should open a prompt box', async () => {
+	let pop = await driver.findElement(By.id('pop'));
+	await pop.click();
+	let alert = await driver.switchTo().alert();
+	await alert.accept();
+});
+
 describe('Clicking "Pusha till stacken"', () => {
 	it('should open a prompt box', async () => {
 		let push = await driver.findElement(By.id('push'));
@@ -41,3 +49,40 @@ describe('Clicking "Pusha till stacken"', () => {
         expect(stack).toEqual("Bananer");
     });
 });
+
+/*NYTT TEST*/
+test('Clicking "Peek" should display the top of the stack', async () => {
+
+	let push = await driver.findElement(By.id('push'));
+    await push.click();
+    let pushAlert = await driver.switchTo().alert();
+    await pushAlert.sendKeys("Oranges");
+    await pushAlert.accept();
+  
+    let peek = await driver.findElement(By.id('peek'));
+    await peek.click();
+    
+    let stack = await driver.findElement(By.id('top_of_stack')).getText();
+    expect(stack).toEqual("Oranges");
+    
+    let pop = await driver.findElement(By.id('pop'));
+    await pop.click();
+    let popAlert = await driver.switchTo().alert();
+    await popAlert.accept();
+});
+
+// NYTT TEST
+test('Make sure all buttons are active', async () => {
+    let pop = await driver.findElement(By.id('pop'));
+    let push = await driver.findElement(By.id('push'));
+    let peek = await driver.findElement(By.id('peek'));
+    
+    expect(await pop.isDisplayed()).toBe(true);
+    expect(await push.isDisplayed()).toBe(true);
+    expect(await peek.isDisplayed()).toBe(true);
+});
+
+
+
+
+
--- tests/stack.test.js
+++ tests/stack.test.js
@@ -18,11 +18,22 @@ test('peek on stack with two or more elements returns the top element', () => {
     expect(stack.peek()).toBe(42);
 });
 
-/* NYTT TEST */
+/* NYA TEST NEDANFÖR */
 test('popping stack with two or more elements returns the elemnt on top of stack and removes it', () => {
     stack.push(1);
     stack.push("wow");
     stack.push(42);
     expect(stack.pop()).toBe(42);
     expect(stack.peek()).toBe("wow");
+});
+
+// NYTT TEST
+test('stack works with last in first out structure', () => {
+    let inputElements = [1, "two", {three: 3}, "four", 5];
+    inputElements.forEach(element => stack.push(element));
+    
+    for(let i = inputElements.length - 1; i >= 0; i--) {
+        let poppedElement = stack.pop();
+        expect(poppedElement).toEqual(inputElements[i]);
+    }
 });
